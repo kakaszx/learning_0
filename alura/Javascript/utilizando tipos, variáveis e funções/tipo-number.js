8a48116bcd7ed2100f0d2859ac3a9c3b15412846
kakaszx <kauaaurelio14@gmail.com>
2025-05-25 15:31:01 -0300

    up

---
+++ alura/Javascript/utilizando tipos, variáveis e funções/tipo-number.js
@@ -0,0 +1,12 @@
+const notaPrimerioBi = 8;
+const notaSegundoBi = 6.3;
+const notaTerceiroBi = -2;
+const notaquartoBi = Number.parseInt("5");
+
+const total =
+  (notaPrimerioBi + notaSegundoBi + notaTerceiroBi + notaquartoBi) / 4;
+
+console.log("a média é " + total.toFixed(2));
+
+const nome = "Kakaszx";
+console.log("meu nome é " + nome);
