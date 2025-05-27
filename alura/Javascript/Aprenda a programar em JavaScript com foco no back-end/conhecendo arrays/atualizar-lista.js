0d568a3d33309cb6c37ff1b54ac244654b719a57
kakaszx <kauaaurelio14@gmail.com>
2025-05-26 23:36:12 -0300

    up

---
+++ alura/Javascript/Aprenda a programar em JavaScript com foco no back-end/conhecendo arrays/atualizar-lista.js
@@ -0,0 +1,6 @@
+const listaEstudantes = ["João", "Ana", "Caio", "Lara", "Marjorie", "Leo"];
+
+listaEstudantes.splice(1, 2, "Rodrigo");
+// listaEstudantes.push("Rodrigo");
+
+console.log(listaEstudantes);
