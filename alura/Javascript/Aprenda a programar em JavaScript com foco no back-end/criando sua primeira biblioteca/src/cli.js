8a4677c42ce9eb92886bb9c8dd4e877729d118fc
kakaszx <kauaaurelio14@gmail.com>
2025-05-30 18:11:44 -0300

    up

--- alura/Javascript/Aprenda a programar em JavaScript com foco no back-end/criando sua primeira biblioteca/src/cli.js
+++ alura/Javascript/Aprenda a programar em JavaScript com foco no back-end/criando sua primeira biblioteca/src/cli.js
@@ -1,15 +1,80 @@
 import fs from "fs";
+import path from "path";
 import trataErros from "./erros/funcoesErro.js";
 import { contaPalavras } from "./index.js";
+import { montaSaidaArquivo } from "./erros/helpers.js";
+import { Command } from "commander";
+import chalk from "chalk";
 
-const caminhoArquivo = process.argv;
-const link = caminhoArquivo[2];
+const program = new Command();
 
-fs.readFile(link, "utf-8", (erro, texto) => {
+program
+  .version("0.0.1")
+  .option("-t, --texto <string>", "caminho do texto a ser processado")
+  .option(
+    "-d, --destino <string>",
+    "caminho da pasta onde salvar o arquivo de resultados"
+  )
+  .action((options) => {
+    const { texto, destino } = options;
+
+    if (!texto || !destino) {
+      console.error(
+        chalk.red("erro: Favor inserir caminho de origem e destino")
+      );
+      program.help();
+      return;
+    }
+
+    const caminhoTexto = path.resolve(texto);
+    const caminhoDestino = path.resolve(destino);
+
+    try {
+      processaArquivo(caminhoTexto, caminhoDestino);
+      console.log(chalk.green("Texto processado com sucesso"));
+    } catch (erro) {
+      console.log("Ocorreu um erro no processamento", erro);
+    }
+  });
+
+program.parse();
+
+function processaArquivo(texto, destino) {
+  fs.readFile(texto, "utf-8", (erro, texto) => {
+    try {
+      if (erro) throw erro;
+      const resultado = contaPalavras(texto);
+      criaESalvaArquivo(resultado, destino);
+    } catch (erro) {
+      trataErros(erro);
+    }
+  });
+}
+
+// USANDO ASYNC AWAIT
+async function criaESalvaArquivo(listaPalavras, endereco) {
+  const arquivoNovo = `${endereco}/resultado.txt`;
+  const textoPalavras = montaSaidaArquivo(listaPalavras);
   try {
-    if (erro) throw erro;
-    contaPalavras(texto);
+    await fs.promises.writeFile(arquivoNovo, textoPalavras);
+    console.log("Arquivo criado!");
   } catch (erro) {
-    trataErros(erro);
+    throw erro;
   }
-});
+}
+
+// USANDO O THEN
+// function criaESalvaArquivo(listaPalavras, endereco) {
+//   const arquivoNovo = `${endereco}/resultado.txt`;
+//   const textoPalavras = montaSaidaArquiv)(listaPalavras)
+
+//   fs.promises
+//     .writeFile(arquivoNovo, textoPalavras)
+//     .then(() => {
+//       console.log("Arquivo criado!");
+//     })
+//     .catch((erro) => {
+//       throw erro;
+//     })
+//     .finally(() => console.log("Operação finalizada!"));
+// }
---
+++ alura/Javascript/Aprenda a programar em JavaScript com foco no back-end/criando sua primeira biblioteca/src/erros/helpers.js
@@ -0,0 +1,17 @@
+// HELPERS FUNCTION
+function filtraOcorrencias(paragrafo) {
+  return Object.keys(paragrafo).filter((chave) => paragrafo[chave] > 1);
+}
+
+function montaSaidaArquivo(listaPalavras) {
+  let textoFinal = "";
+  listaPalavras.forEach((paragrafo, indice) => {
+    const duplicadas = filtraOcorrencias(paragrafo).join(", ");
+    textoFinal += `palavras duplicas no parágrafo ${
+      indice + 1
+    }: ${duplicadas} \n`;
+  });
+  return textoFinal;
+}
+
+export { montaSaidaArquivo };
