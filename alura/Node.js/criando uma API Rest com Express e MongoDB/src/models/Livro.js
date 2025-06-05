645e89649e917baebf8cf9166d387f87454198d1
kakaszx <kauaaurelio14@gmail.com>
2025-06-04 23:20:59 -0300

    up

---
+++ alura/Node.js/criando uma API Rest com Express e MongoDB/src/models/Livro.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { autorSchema } from "./Autor.js";
+
+const livroSchema = new mongoose.Schema(
+  {
+    id: { type: mongoose.Schema.Types.ObjectId },
+    titulo: { type: String, required: true },
+    autor: autorSchema,
+    editora: { type: String },
+    preco: { type: Number },
+    paginas: { type: Number },
+  },
+  { versionKey: false }
+);
+
+const livro = mongoose.model("livros", livroSchema);
+
+export default livro;
