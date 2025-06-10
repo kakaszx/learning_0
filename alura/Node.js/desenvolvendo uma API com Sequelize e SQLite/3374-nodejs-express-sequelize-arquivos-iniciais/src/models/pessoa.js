fcefd1279f905b77742e013fdf590a3cb05d00fc
kakaszx <kauaaurelio14@gmail.com>
2025-06-10 10:31:28 -0300

    up

--- alura/Node.js/desenvolvendo uma API com Sequelize e SQLite/3374-nodejs-express-sequelize-arquivos-iniciais/src/models/pessoa.js
+++ alura/Node.js/desenvolvendo uma API com Sequelize e SQLite/3374-nodejs-express-sequelize-arquivos-iniciais/src/models/pessoa.js
@@ -1,14 +1,14 @@
-'use strict';
-const { Model } = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Pessoa extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Pessoa.hasMany(models.Curso, {
+        foreignKey: "docente_id",
+      });
+      Pessoa.hasMany(models.Matricula, {
+        foreignKey: "estudante_id",
+      });
     }
   }
   Pessoa.init(
@@ -21,8 +21,8 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       sequelize,
-      modelName: 'Pessoa',
-      tableName: 'pessoas',
+      modelName: "Pessoa",
+      tableName: "pessoas",
     }
   );
   return Pessoa;
