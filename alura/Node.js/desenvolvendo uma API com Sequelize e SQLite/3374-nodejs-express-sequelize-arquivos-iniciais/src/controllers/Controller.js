e76ece211737c58b3c535f3f38746f8eeddfd5c4
kakaszx <kauaaurelio14@gmail.com>
2025-06-10 12:14:09 -0300

    up

--- alura/Node.js/desenvolvendo uma API com Sequelize e SQLite/3374-nodejs-express-sequelize-arquivos-iniciais/src/controllers/Controller.js
+++ alura/Node.js/desenvolvendo uma API com Sequelize e SQLite/3374-nodejs-express-sequelize-arquivos-iniciais/src/controllers/Controller.js
@@ -12,32 +12,36 @@ class Controller {
     }
   }
 
-  // async pegaUmPorId(req, res) {
-  //   const { id } = req.params;
-  //   try {
-  //     const umRegistro = await this.entidadeService.pegaUmRegistroPorId(Number(id));
-  //     return res.status(200).json(umRegistro);
-  //   } catch (erro) {
-  //     // erro
-  //   }
-  // }
+  async pegaUmPorId(req, res) {
+    const { id } = req.params;
+    try {
+      const umRegistro = await this.entidadeService.pegaUmRegistroPorId(
+        Number(id)
+      );
+      return res.status(200).json(umRegistro);
+    } catch (erro) {
+      // erro
+    }
+  }
 
-  // async criaNovo(req, res) {
-  //   const dadosParaCriacao = req.body;
-  //   try {
-  //     const novoRegistroCriado = await this.entidadeService.criaRegistro(dadosParaCriacao);
-  //     return res.status(200).json(novoRegistroCriado);
-  //   } catch (erro) {
-  //     // erro
-  //   }
-  // }
+  async criaNovo(req, res) {
+    const dadosParaCriacao = req.body;
+    try {
+      const novoRegistroCriado = await this.entidadeService.criaRegistro(
+        dadosParaCriacao
+      );
+      return res.status(200).json(novoRegistroCriado);
+    } catch (erro) {
+      // erro
+    }
+  }
 
   async atualiza(req, res) {
     const { id } = req.params;
     const dadosAtualizados = req.body;
     try {
       //isUpdated
-      const foiAtualizado = await this.entidadeService.atualizaRegistro(
+      const foiAtualizado = this.entidadeService.atualizaRegistro(
         dadosAtualizados,
         Number(id)
       );
@@ -52,16 +56,15 @@ class Controller {
     }
   }
 
-  // async exclui(req, res) {
-  //   const { id } = req.params;
-  //   try {
-  //     await this.entidadeService.excluiRegistro(Number(id));
-  //     return res.status(200).json({ mensagem: `id ${id} deletado` });
-
-  //   } catch (error) {
-  //     return res.status(500).json(error.message);
-  //   }
-  // }
+  async exclui(req, res) {
+    const { id } = req.params;
+    try {
+      await this.entidadeService.excluiRegistro(Number(id));
+      return res.status(200).json({ mensagem: `id ${id} deletado` });
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
 }
 
 module.exports = Controller;
