fc46a3b3d8e08d0bcbef49de5c2be4696286c375
kakaszx <kauaaurelio14@gmail.com>
2025-06-17 20:24:42 -0300

    up

---
+++ alura/React/hooksdoreactuseeffect/src/App.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [racas, setRacas] = useState([]);
+  const [busca, setBusca] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/doguinhos").then((resposta) =>
+      resposta.json().then((dados) => {
+        setRacas(dados);
+      })
+    );
+  }, []);
+
+  useEffect(() => {
+    if (busca && busca.length > 3) {
+      fetch("http://localhost:8000/doguinhos?nome=" + busca).then((resposta) =>
+        resposta.json().then((dados) => {
+          setRacas(dados);
+        })
+      );
+    }
+  }, [busca]);
+
+  return (
+    <div className="App">
+      <h1>Bem vindo aos doguinhos!</h1>
+      <h4>Confira abaixo uma lista de raças dos doguinhos</h4>
+      <input
+        placeholder="Buscar por raça"
+        onChange={(evento) => setBusca(evento.target.value)}
+      />
+      <ul>
+        {racas.map((raca) => (
+          <li key={raca.nome}>{raca.nome} </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
