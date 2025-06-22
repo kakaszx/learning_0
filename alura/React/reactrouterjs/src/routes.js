b70f5500eff3e43edef2bc93e78daa6d94105a6a
kakaszx <kauaaurelio14@gmail.com>
2025-06-22 15:54:44 -0300

    up

---
+++ alura/React/reactrouterjs/src/routes.js
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Inicio from "./paginas/Inicio";
+import SobreMim from "./paginas/SobreMim";
+import Menu from "./componentes/Menu";
+import Rodape from "componentes/Rodape";
+import PaginaPadrao from "componentes/PaginaPadrao";
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Menu />
+      <Routes>
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route path="/" element={<Inicio />} />
+          <Route path="/sobremim" element={<SobreMim />} />
+        </Route>
+        <Route path="*" element={<div>Página não encontada.</div>} />
+      </Routes>
+      <Rodape />
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
