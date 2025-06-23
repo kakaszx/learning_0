08da461739a5b1e1e21ab1302ab18214ea8903ba
kakaszx <kauaaurelio14@gmail.com>
2025-06-23 00:48:13 -0300

    up

--- alura/React/reactrouterjs/src/routes.js
+++ alura/React/reactrouterjs/src/routes.js
@@ -4,17 +4,22 @@ import SobreMim from "./paginas/SobreMim";
 import Menu from "./componentes/Menu";
 import Rodape from "componentes/Rodape";
 import PaginaPadrao from "componentes/PaginaPadrao";
+import Post from "paginas/Post";
+import NaoEncontrada from "paginas/NaoEncontrada";
+import ScrollToTop from "componentes/ScrollToTop";
 
 function AppRoutes() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Menu />
       <Routes>
         <Route path="/" element={<PaginaPadrao />}>
           <Route path="/" element={<Inicio />} />
           <Route path="/sobremim" element={<SobreMim />} />
         </Route>
-        <Route path="*" element={<div>Página não encontada.</div>} />
+        <Route path="posts/:id" element={<Post />} />
+        <Route path="*" element={<NaoEncontrada />} />
       </Routes>
       <Rodape />
     </BrowserRouter>
