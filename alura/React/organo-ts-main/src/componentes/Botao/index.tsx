fc3ad3c20f4bb8e0bdf2614c86179ade8f2b8cc4
kakaszx <kauaaurelio14@gmail.com>
2025-06-27 14:51:23 -0300

    up

--- alura/React/organo-ts-main/src/componentes/Botao/index.tsx
+++ alura/React/organo-ts-main/src/componentes/Botao/index.tsx
@@ -1,16 +1,12 @@
-import { ReactElement } from 'react'
-import './Botao.css'
-
-import React from "react"
+import { ReactElement } from "react";
+import "./Botao.css";
 
 interface BotaoProps {
-    children: ReactElement
+  children: ReactElement | string;
 }
 
 const Botao = (props: BotaoProps) => {
-    return (<button className='botao'>
-        {props.children}
-    </button>)
-}
+  return <button className="botao">{props.children}</button>;
+};
 
-export default Botao
+export default Botao;
