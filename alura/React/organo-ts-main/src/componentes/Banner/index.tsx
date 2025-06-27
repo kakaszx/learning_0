a9d9e84e66419e0f63db8690c4467e14a4e5d195
kakaszx <kauaaurelio14@gmail.com>
2025-06-26 22:12:43 -0300

    up

---
+++ alura/React/organo-ts-main/src/componentes/Banner/index.tsx
@@ -0,0 +1,23 @@
+import "./Banner.css";
+
+import React from "react";
+
+interface BannerProps {
+  enderecoImagem: string;
+  textoAlternativo?: string;
+}
+
+export const Banner = ({ enderecoImagem, textoAlternativo }: BannerProps) => {
+  // JSX
+  return (
+    <header className="banner">
+      {/* <img
+        src="/imagens/banner.png"
+        alt="O banner principal da página do Organo"
+      /> */}
+      <img src={enderecoImagem} alt={textoAlternativo} />
+    </header>
+  );
+};
+
+export default Banner;
